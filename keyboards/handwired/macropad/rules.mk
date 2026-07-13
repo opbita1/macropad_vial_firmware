39541313b8bd84bb800ef9576b69f54048ceec7b
opbita1 <119452155+opbita1@users.noreply.github.com>
2026-07-13 12:42:29 +0500

    Update rules.mk

--- keyboards/handwired/macropad/rules.mk
+++ keyboards/handwired/macropad/rules.mk
@@ -1,1 +1,10 @@
+# Основные параметры для RP2040
+MCU = rp2040
+BOOTLOADER = rp2040
+
+# Включение функций Vial
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# Опционально: оптимизация размера кода (полезно для макропадов)
 LTO_ENABLE = yes
